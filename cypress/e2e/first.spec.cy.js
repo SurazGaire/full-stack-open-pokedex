6c818db576b4fee7d33f7dab8855e83d9046dbba
SurazGaire <luxheartsuraz@gmail.com>
2022-09-14 13:36:09 +0545

    feat : added new test in cypress

--- cypress/e2e/first.spec.cy.js
+++ cypress/e2e/first.spec.cy.js
@@ -10,5 +10,10 @@ describe('empty spec', () => {
         'Pokémon and Pokémon character names are trademarks of Nintendo.'
       )
     })
+    it('one can navigate from the main page to the page of a particular Pokemon', function () {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur').click()
+      cy.contains('chlorophyll')
+    })
   })
 })
